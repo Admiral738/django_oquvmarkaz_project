fb5ffa947e4ad344e0a409117585a9b38ccd2c2c
samadbek1998 <72186888+Admiral738@users.noreply.github.com>
2021-08-08 20:27:13 +0500

    Commit

--- oquvmarkaz/settings.py
+++ oquvmarkaz/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$hq5wi$bn(3=kdy88(&#+(&(-3!0&ojcj$^nrpg)*y$=1hz$a6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1' 'mentordjangoapp.herokuapp.com']
+
 
 
 # Application definition
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
